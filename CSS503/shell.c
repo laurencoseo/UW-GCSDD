f19a6692d948863dc3ddd43900a777118ea96f05
Lauren Coseo <laurencoseo@gmail.com>
2022-04-10 10:50:14 -0700

    Add getline and strtok

--- CSS503/shell.c
+++ CSS503/shell.c
@@ -19,8 +19,34 @@ int main(void) {
         printf("osh>");
         fflush(stdout);
 
+        // size_t getline(char **lineptr, size_t *n, FILE *stream);
+
+        int bytes_read;
+        int size = 20;
+        char *string;
+
+        /* These 2 lines are very important. */
+        string = (char *) malloc (size);
+        bytes_read = getline (&string, &size, stdin);
+
+        if (bytes_read == -1) {
+            // puts ("ERROR!");
+        }
+
         // getline - get line from terminal
         // strtok - tokenize the string into the args char array
+
+        const char s[2] = " ";
+        char *token;
+
+        /* get the first token */
+        token = strtok(str, s);
+
+        /* walk through other tokens */
+        while( token != NULL ) {
+            printf( " %s\n", token );
+            token = strtok(NULL, s);
+        }
 
         //* After reading user input, the steps are:
         //* (1) fork a child process using fork()
@@ -28,6 +54,7 @@ int main(void) {
         //* (3) parent will invoke wait() unless command included & */
 
         // if exit is typed, should_run = 0
+        should_run = 0;
     }
 
     return 0;
