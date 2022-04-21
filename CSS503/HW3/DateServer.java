bd30926f9e6b8b6a72dc392c00139be90ab91937
Lauren Coseo <laurencoseo@gmail.com>
2022-04-20 20:51:06 -0700

    Update for multithread

--- CSS503/HW3/DateServer.java
+++ CSS503/HW3/DateServer.java
@@ -1,24 +1,47 @@
 /**
- * Created by rtdimpsey on 4/13/17.
+ * Created by rtdimpsey and lcoseo on 4/13/17 and 4/20/22.
  */
 import java.net.*;
 import java.io.*;
 
 public class DateServer
 {
+    public class Worker implements Runnable
+    {
+        Socket cl;
+
+        public Worker(Socket client) {
+            cl = client;
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                try {
+                    PrintWriter pout = new PrintWriter(cl.getOutputStream(), true);
+                    pout.println(new java.util.Date().toString());
+                    cl.close();
+                } catch (IOException ioe) {}
+
+            }
+        }
+    }
+
     public static void main(String[] args)
     {
         try
         {
             ServerSocket sock = new ServerSocket(6013);
+
             while (true)
             {
                 Socket client = sock.accept();
-                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-                pout.println(new java.util.Date().toString());
-                client.close();
+                Thread worker = new Thread(new DateServer().new Worker(client));
+                worker.start();
             }
         }
         catch (IOException ie) {}
     }
 }
+
